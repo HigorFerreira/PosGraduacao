d392eaa6e7bb88b14fc3ecf65e67e035e7e53d43
Higor <hfashigor@hotmail.com>
2025-05-23 15:15:37 -0300

    aDJUSTS

--- InteligenciaArtificial/Hanoi.py
+++ InteligenciaArtificial/Hanoi.py
@@ -1,3 +1,18 @@
+
+from typing import Literal
+
+
+Block=Literal[1] | Literal[2] | Literal[3] | Literal[4]
+
 class HanoiState:
-    def __init__(self, ):
-        pass
+    towers: list[list[Block]]
+    def __init__(self, towerA: list[Block], towerB: list[Block], towerC: list[Block]):
+        self.towers = [
+            towerA,
+            towerB,
+            towerC,
+        ]
+
+    def isValid() -> bool:
+        
+        return True
