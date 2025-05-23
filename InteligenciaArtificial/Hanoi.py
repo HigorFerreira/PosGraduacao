2c1d17dfe6467844575cbe7426d49fc844e743e9
Higor <hfashigor@hotmail.com>
2025-05-23 16:51:02 -0300

    Adjusts

--- InteligenciaArtificial/Hanoi.py
+++ InteligenciaArtificial/Hanoi.py
@@ -2,17 +2,32 @@
 from typing import Literal
 
 
-Block=Literal[1] | Literal[2] | Literal[3] | Literal[4]
+Block=Literal[1, 2, 3, 4]
 
 class HanoiState:
     towers: list[list[Block]]
-    def __init__(self, towerA: list[Block], towerB: list[Block], towerC: list[Block]):
+    def __init__(self, towerA: list[Block] = [], towerB: list[Block] = [], towerC: list[Block] = []):
         self.towers = [
             towerA,
             towerB,
             towerC,
         ]
 
-    def isValid() -> bool:
-        
+    def isValid(self) -> bool:
+        for tower in self.towers:
+            for i, block in enumerate(tower):
+                if i == 0: continue
+                if block > tower[i-1]: return False
         return True
+
+
+print(HanoiState([ 3, 2, 1 ]).isValid())
+print(HanoiState([ 4, 3, 2, 1 ]).isValid())
+print(HanoiState([ 3, 1, 2 ]).isValid())
+print(HanoiState([ 1, 2, 3 ]).isValid())
+
+# Approved test
+
+class Tree:
+    def __init__(self) -> None:
+        pass
