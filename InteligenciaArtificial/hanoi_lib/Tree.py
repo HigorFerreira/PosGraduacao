b17dcdb2709c4e54a3e08aa9fd646926395c0989
Higor Ferreira <hfashigor@hotmail.com>
2025-05-25 14:13:55 -0300

    Not working

--- InteligenciaArtificial/hanoi_lib/Tree.py
+++ InteligenciaArtificial/hanoi_lib/Tree.py
@@ -6,7 +6,7 @@ class Tree:
     def __init__(self, root: Node) -> None:
         self.root = root
 
-    def mountTree(self, base: Node, level=1, maxLevel=12):
+    def mountTree(self, base: Node, level=1, maxLevel=10):
         if level == maxLevel: return
         ways = base.get_state().generatePossibleWays()
         ways = list(map(lambda x: Node(x), ways))
