0b050d4ccf34d77089642132fdf3b85a829dca09
Higor Ferreira <hfashigor@hotmail.com>
2025-05-25 11:16:10 -0300

    Not working correcly yet

--- InteligenciaArtificial/Hanoi.py
+++ InteligenciaArtificial/Hanoi.py
@@ -9,16 +9,18 @@ print("Tree mountted")
 
 def goalFunction(node: Node):
     towerA = node.get_state().towers[0]
-    towerB = node.get_state().towers[0]
-    towerC = node.get_state().towers[0]
+    towerB = node.get_state().towers[1]
+    towerC = node.get_state().towers[2]
 
-    return (len(towerA) == 0 and len(towerB) == 0) or (len(towerA) == 0 and len(towerC) == 0)
+    return len(towerA) == 0 and len(towerB) == 0 or len(towerA) == 0 and len(towerC) == 0
 
 print("Finding a path...")
 path = tree.dfs(tree.root, lambda node: goalFunction(node))
-print("Path found")
+if len(path) != 0: print("Path found")
+else: print("No path found")
 
-os.mkdir("path", 0o777)
+if not os.path.exists(os.path.join(os.getcwd(), 'path')):
+    os.mkdir("path", 0o777)
 for i, step in enumerate(path):
     with open(f"path/step-{i+1}.svg", "w") as f:
         f.write(step.get_state().generateImage(label=f"Passo {i+1}"))
--- InteligenciaArtificial/hanoi_lib/Tree.py
+++ InteligenciaArtificial/hanoi_lib/Tree.py
@@ -6,7 +6,7 @@ class Tree:
     def __init__(self, root: Node) -> None:
         self.root = root
 
-    def mountTree(self, base: Node, level=1, maxLevel=10):
+    def mountTree(self, base: Node, level=1, maxLevel=12):
         if level == maxLevel: return
         ways = base.get_state().generatePossibleWays()
         ways = list(map(lambda x: Node(x), ways))
@@ -23,6 +23,7 @@ class Tree:
         stack.append(start_node)
         
         while True:
+            if len(stack) == 0: return []
             node = stack.pop()
             if node.get_state().serialize() in visited: continue
             if not node.get_state().isValid(): continue
