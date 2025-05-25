050b46196b4343e137020feff3fb8e9f9cfba6cc
Higor Ferreira <hfashigor@hotmail.com>
2025-05-25 11:00:08 -0300

    Working not optimal

--- InteligenciaArtificial/Hanoi.py
+++ InteligenciaArtificial/Hanoi.py
@@ -1,20 +1,24 @@
 from hanoi_lib import HanoiState, Tree, Node
+import os
 
 tree = Tree(Node(HanoiState([ 3, 2, 1 ])))
 
+print("Mounting tree...")
 tree.mountTree(tree.root)
+print("Tree mountted")
 
 def goalFunction(node: Node):
     towerA = node.get_state().towers[0]
     towerB = node.get_state().towers[0]
     towerC = node.get_state().towers[0]
 
-    return len(towerA) == 0 and len(towerB) == 0\
-        or len(towerA) == 0 and len(towerC) == 0
+    return (len(towerA) == 0 and len(towerB) == 0) or (len(towerA) == 0 and len(towerC) == 0)
 
-
+print("Finding a path...")
 path = tree.dfs(tree.root, lambda node: goalFunction(node))
-print("\n\nPATH", path)
+print("Path found")
 
-with open('test.svg', 'w') as f: f.write(path[0].get_state().generateImage())
-with open('test1.svg', 'w') as f: f.write(path[-1].get_state().generateImage())
+os.mkdir("path", 0o777)
+for i, step in enumerate(path):
+    with open(f"path/step-{i+1}.svg", "w") as f:
+        f.write(step.get_state().generateImage(label=f"Passo {i+1}"))
