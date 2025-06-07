787c29f2ed244043697ee1737d84684274aff592
Higor Ferreira <hfashigor@hotmail.com>
2025-06-07 13:19:06 -0300

    Going well

---
+++ Programacao/AtividadeProgFuncional/my_utils/print_dict.py
@@ -0,0 +1,20 @@
+from typing import Callable, Any
+from functools import wraps
+
+
+def print_dict(print_fn):
+    def decorator(func: Callable[[], dict]):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        
+        _dict = wrapper()
+        for key, value in _dict.items():
+            if print_fn is None:
+                print(f"{key} ==> {value}")
+            else:
+                print(print_fn(key, value))
+
+        return wrapper
+    
+    return decorator
